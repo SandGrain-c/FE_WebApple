aa1c4582b4c074cac089a87eaa8a424f6d52f40d
Vananh250804 <148927815+Vananh250804@users.noreply.github.com>
2026-07-30 23:46:19 +0700

    fix: remove runtime dependency on remote fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "swiper/css";
 import "swiper/css/navigation";
 import "./globals.css";
 import ToastContainer from "@/components/ui/ToastContainer";
 import AppLoadingOverlay from "@/components/common/loading/AppLoadingOverlay";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "Đức Bách Hoá",
   description: "Chuyên sản phẩm Apple chính hãng",
